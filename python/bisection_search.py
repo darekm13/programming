52ea32c7116350de476c1983d58b05dc00ea0a0b
Dariusz Maciocha <darek.maciocha@gmail.com>
2015-09-04 14:36:26 +0200

    algorytm binarny w uzyciu ver. 1.3

--- python/bisection_search.py
+++ python/bisection_search.py
@@ -2,61 +2,63 @@ __author__ = 'daro'
 from math import log
 
 
-def find_root(x):
+def find_root(x, power, epsilon = 0.0001):
     """
     Pierwiastek z x metoda binarna
     Input:
-    :param x: Dowolna liczba nieujemna
-    :return: Pierwiastek z x zaokraglony do 4 miejsc po przecinku z bledem epsilon
+    :param x: float, Liczba z ktorej szukamy pierwiastek
+    :param power: int, Potega liczby, power >= 0
+    :param epsilon: Dokladnosc wyniku, epsilon > 0 dla float i epsilon >= 0 dla int
+    :return: Pierwiastek potegi power z x zaokraglony do 4 miejsc po przecinku
     """
     try:
         x = float(x)
+        power = int(power)
+        epsilon = float(epsilon)
     except ValueError:
         print('Podano zla wartosc zmiennej do funkcji')
-    if x < 0:
+    if power == 0:
+        return 1
+    if power % 2 == 0 and x < 0 and power != 0:
         print('Podana zla wartosc')
         exit(1)
 
-    min = 0
-    max = x
-    epsilon = 0.0001
-    ans = (min + max) / 2
-    while abs(ans**2 - x) > epsilon:
-        if ans**2 > x:
-            max = ans
+    low = min(-1, x)
+    high = max(1, x)
+    ans = (low + high) / 2.0
+    while abs(ans**power - x) > epsilon:
+        if ans**power > x:
+            high = ans
         else:
-            min = ans
-        ans = (min + max) / 2
+            low = ans
+        ans = (low + high) / 2
     return round(ans, 4)
 
 
-def bisection_search_game(min = 0, max = 100):
+def bisection_search_game(low = 0, high = 100):
     """
     Gra
     Input (opcjonalnie):
-    :param min: minimalna wartosc przedzialu poszukiwac
-    :param max: maksymalna wartosc przedzialu poszukiwan - 1
+    :param low: minimalna wartosc przedzialu poszukiwac
+    :param high: maksymalna wartosc przedzialu poszukiwan - 1
     """
     try:
-        min = int(min)
-        max = int(max)
+        low = int(min(low, high))
+        high = int(max(low, high))
     except ValueError:
          print('Podano zla wartosc zmiennej do funkcji')
-    if min > max:
-        print('Wartosc minimalna nie moze byc wieksza od maksymalnej')
-        exit(1)
 
-    tries = int(log(max - min, 2)) + 1
-    print('Pomysl liczbe z przedzialu <%d, %d>. Mam %d prob na trafienie.' % (min, max-1, tries))
+    tries = int(log(high - low, 2)) + 1
+    print('Pomysl liczbe z przedzialu <%d, %d>. Mam %d prob na trafienie.' % (low, high-1, tries))
     while tries > 0:
-        guess = (min + max) / 2
+        guess = (low + high) / 2
         print('\nPozostalo %d strzalow. Czy Twoja liczba to %s' % (tries, guess))
         ans = raw_input("Wprowadz 'l' jesli za mala, 'h' jesli za duza lub 'c' jesli zgadlem:  ")
         if ans == 'l':
-            min = guess
+            low = guess
             tries -= 1
         elif ans == 'h':
-            max = guess
+            high = guess
             tries -= 1
         elif ans == 'c':
             print('Koniec gry. Twoja liczba to %d' % guess)
@@ -69,4 +71,5 @@ def bisection_search_game(min = 0, max = 100):
 
 bisection_search_game()
 number = raw_input('Wprowadz liczbe: ')
-print('Pierwiastek z ' + str(number) + ' to ' + str(find_root(number)))
+power = raw_input('Wprowadz potege: ')
+print('Pierwiastek z ' + str(number) + ' to ' + str(find_root(number, power)))
